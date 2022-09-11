23e6d2fb75f6148ff7ecd633821844e6681f6909
Laura <96611296+Lourioo@users.noreply.github.com>
2022-09-11 19:32:21 +0600

    Delete Practice 1.py

--- Practice 1.py
+++
@@ -1,7 +0,0 @@
-name = input("What's your first and last name? ")
-age = input('How old are you? ')
-place = input('Where do you live? ')
-print('Your name is ', name)
-print('Your age ', age)
-print('You live in ', place)
-#Sepbossynova Laura CS-2120
